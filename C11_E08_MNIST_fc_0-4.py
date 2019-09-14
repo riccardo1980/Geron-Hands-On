c31305e9abe9fe63cb389b989042cc24dd3e0a1f
Riccardo Zanella <riccardo.zanella@gmail.com>
2019-09-14 16:20:49 +0200

    argument parsing + linter

--- C11_E08_MNIST_fc_0-4.py
+++ C11_E08_MNIST_fc_0-4.py
@@ -1,101 +1,138 @@
 import os
+import sys
 import warnings
+import argparse
+from datetime import datetime
 import numpy as np
 
-with warnings.catch_warnings():  
-    warnings.filterwarnings("ignore",category=FutureWarning)
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=FutureWarning)
     import tensorflow as tf
 
     from C11_resources import FullyconnectedClassifier
 
-from datetime import datetime
-
 def dataset_prune(features, labels, idx):
     return features[idx], labels[idx]
 
-# read complete dataset
-(complete_train_data, complete_train_target), (complete_test_data, complete_test_target) = tf.keras.datasets.mnist.load_data()
+def get_data():
+    # read complete dataset
+    (complete_train_data, complete_train_target), (complete_test_data, complete_test_target) = tf.keras.datasets.mnist.load_data()
 
-# retain digits 0 to 4
-train_data, train_target = dataset_prune(complete_train_data, 
-                                         complete_train_target,
-                                         (complete_train_target < 5))
+    # retain digits 0 to 4
+    train_data, train_target = dataset_prune(complete_train_data,
+                                             complete_train_target,
+                                             (complete_train_target < 5))
 
-test_data, test_target = dataset_prune(complete_test_data,
-                                       complete_test_target,
-                                       (complete_test_target < 5))
+    test_data, test_target = dataset_prune(complete_test_data,
+                                           complete_test_target,
+                                           (complete_test_target < 5))
 
-# cast features to float32, targets to int32
-train_data = train_data.astype(np.float32)
-test_data = test_data.astype(np.float32) 
-train_target = train_target.astype(np.int32)
-test_target = test_target.astype(np.int32) 
+    # cast features to float32, targets to int32
+    train_data = train_data.astype(np.float32)
+    test_data = test_data.astype(np.float32)
+    train_target = train_target.astype(np.int32)
+    test_target = test_target.astype(np.int32)
 
-# standardization trained on train, reused on test dataset
+    return train_data, test_data, train_target, test_target
+
 def preproc_standardize(dataset, mean, std):
     return (dataset - mean) / (std + 1e-15)
 
-features_mean = np.mean(train_data, axis=0)
-features_std = np.std(train_data, axis=0)
 
-X_train = preproc_standardize(train_data, features_mean, features_std)
-X_test =  preproc_standardize(test_data, features_mean, features_std)
+def main(_):
 
-y_train = train_target
-y_test = test_target
+    train_data, test_data, y_train, y_test = get_data()
 
-n_classes = len(set(train_target))
+    # standardization trained on train, reused on test dataset
+    features_mean = np.mean(train_data, axis=0)
+    features_std = np.std(train_data, axis=0)
 
-batch_size=200
-max_steps=30000
-save_checkpoints_steps=2000 # checkpoints
-log_step_count_steps=100    # summary
-throttle_secs = 10          # do not re-evaluate unless last eval is lolder than...
-learning_rate = 1e-4
+    X_train = preproc_standardize(train_data, features_mean, features_std)
+    X_test = preproc_standardize(test_data, features_mean, features_std)
 
-print('n_classes: {}'.format(n_classes))
-print('epochs: {}'.format( max_steps * batch_size / y_train.shape[0] ))
+    n_classes = len(set(y_train))
 
-# this seed is used only for initialization
-# batch is still random with no chance to set the seed
-# see: https://stackoverflow.com/questions/47009560/tf-estimator-shuffle-random-seed
-config = tf.estimator.RunConfig(tf_random_seed=42,
-                                model_dir=os.path.join('tmp',
-                                                       datetime.utcnow().strftime('%Y%m%d-%H%M%S')),
-                                save_checkpoints_steps=save_checkpoints_steps,
-                                log_step_count_steps=log_step_count_steps)
+    print('n_classes: {}'.format(n_classes))
+    print('epochs: {}'.format(FLAGS.max_steps * FLAGS.train_batch_size / y_train.shape[0]))
 
-# create feature columns
-feature_columns = [ tf.feature_column.numeric_column(key='f1', shape=X_train[0].shape) ]
+    # this seed is used only for initialization
+    # batch is still random with no chance to set the seed
+    # see: https://stackoverflow.com/questions/47009560/tf-estimator-shuffle-random-seed
+    config = tf.estimator.RunConfig(tf_random_seed=42,
+                                    model_dir=os.path.join('tmp',
+                                                           datetime.utcnow().strftime('%Y%m%d-%H%M%S')),
+                                    save_checkpoints_steps=FLAGS.save_checkpoints_steps,
+                                    log_step_count_steps=FLAGS.log_step_count_steps)
 
-params = {'feature_columns': feature_columns,
-    'hidden_units': 5*[100],
-    'activation': 'elu',
-    'n_classes': n_classes,
-    'optimizer': tf.compat.v1.train.AdagradOptimizer(learning_rate=learning_rate),
-    'batch_norm_momentum': 0.9
-}
+    # create feature columns
+    feature_columns = [tf.feature_column.numeric_column(key='f1', shape=X_train[0].shape)]
 
-estimator = tf.estimator.Estimator(
-    model_fn=FullyconnectedClassifier.model_fn,
-    params=params,
-    config=config
+    params = {'feature_columns': feature_columns,
+              'hidden_units': 5*[100],
+              'activation': 'elu',
+              'n_classes': n_classes,
+              'optimizer': tf.compat.v1.train.AdagradOptimizer(learning_rate=FLAGS.learning_rate),
+              'batch_norm_momentum': 0.9
+             }
+
+    estimator = tf.estimator.Estimator(
+        model_fn=FullyconnectedClassifier.model_fn,
+        params=params,
+        config=config
+        )
+
+    train_spec = tf.estimator.TrainSpec(
+        input_fn=tf.compat.v1.estimator.inputs.numpy_input_fn(x={'f1' : X_train},
+                                                              y=y_train,
+                                                              batch_size=FLAGS.train_batch_size,
+                                                              num_epochs=None,
+                                                              shuffle=False),
+        max_steps=FLAGS.max_steps)
+
+    eval_spec = tf.estimator.EvalSpec(
+        input_fn=tf.compat.v1.estimator.inputs.numpy_input_fn(x={'f1' : X_test},
+                                                              y=y_test,
+                                                              batch_size=len(y_test),
+                                                              num_epochs=None,
+                                                              shuffle=False),
+        throttle_secs=FLAGS.throttle_secs)
+
+    tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.register("type", "bool", lambda v: v.lower() == "true")
+
+    parser.add_argument(
+        "--max_steps", type=int, default=30000,
+        help="Number of steps to run trainer."
     )
 
-train_spec = tf.estimator.TrainSpec(
-    input_fn=tf.compat.v1.estimator.inputs.numpy_input_fn(x = {'f1' : X_train},
-                                                          y = y_train,
-                                                          batch_size=batch_size,
-                                                          num_epochs=None,
-                                                          shuffle=False), 
-    max_steps=max_steps)                        
+    parser.add_argument(
+        "--train_batch_size", type=int, default=200,
+        help="Batch size used during training."
+    )
 
-eval_spec = tf.estimator.EvalSpec(
-    input_fn=tf.compat.v1.estimator.inputs.numpy_input_fn(x = {'f1' : X_test},
-                                                          y = y_test,
-                                                          batch_size = len(y_test),
-                                                          num_epochs=None,
-                                                          shuffle=False),
-    throttle_secs=throttle_secs)
+    parser.add_argument(
+        "--learning_rate", type=float, default=1e-4,
+        help="Initial learning rate."
+    )
 
-tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+    parser.add_argument(
+        "--save_checkpoints_steps", type=int, default=2000,
+        help="Save checkpoints every this many steps."
+    )
+
+    parser.add_argument(
+        "--log_step_count_steps", type=int, default=100,
+        help="Log and summary frequency, in global steps."
+    )
+
+    parser.add_argument(
+        "--throttle_secs", type=int, default=10,
+        help="Evaluation throttle in seconds."
+    )
+
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+    
--- C11_resources/FullyconnectedClassifier.py
+++ C11_resources/FullyconnectedClassifier.py
@@ -1,43 +1,43 @@
-import numpy as np 
+import numpy as np
 import tensorflow as tf
 
 from tensorflow.python.keras import activations
 
 def neuron_layer(X, units, name, mode, activation=None,
-                batch_norm_momentum=None):
+                 batch_norm_momentum=None):
     with tf.name_scope(name):
-        
+
         inputs = int(np.prod(X.get_shape().as_list()[1:]))
         stddev = 2 / np.sqrt(inputs + units)
-        
+
         if batch_norm_momentum is not None:
             # Dense + normalization + activation
             Z = tf.keras.layers.Dense(units,
-                                  activation=None, 
-                                  use_bias=True,
-                                  kernel_initializer=tf.initializers.truncated_normal(stddev=stddev),
-                                  bias_initializer='zeros')(X)
+                                      activation=None,
+                                      use_bias=True,
+                                      kernel_initializer=tf.initializers.truncated_normal(stddev=stddev),
+                                      bias_initializer='zeros')(X)
             Z = tf.layers.batch_normalization(Z,
-                                              training=mode==tf.estimator.ModeKeys.TRAIN,
+                                              training=mode == tf.estimator.ModeKeys.TRAIN,
                                               momentum=batch_norm_momentum)
             if activation is not None:
                 Z = activations.get(activation)(Z)
         else:
             # Dense with activation
             Z = tf.keras.layers.Dense(units,
-                                  activation=activation, 
-                                  use_bias=True,
-                                  kernel_initializer=tf.initializers.truncated_normal(stddev=stddev),
-                                  bias_initializer='zeros')(X)
-            
+                                      activation=activation,
+                                      use_bias=True,
+                                      kernel_initializer=tf.initializers.truncated_normal(stddev=stddev),
+                                      bias_initializer='zeros')(X)
+
         return Z
 
 def model_fn(features, labels, mode, params):
 
     # input
     net = tf.compat.v1.feature_column.input_layer(features,
-                                        params['feature_columns'])
-    
+                                                  params['feature_columns'])
+
     # hidden layers
     for idx, units in enumerate(params['hidden_units']):
         net = neuron_layer(net, units, name='dense_'+str(idx+1), mode=mode,
@@ -52,26 +52,26 @@ def model_fn(features, labels, mode, params):
     predicted_classes = tf.argmax(logits, 1)
 
     if mode == tf.estimator.ModeKeys.PREDICT:
-        predictions = { 
+        predictions = {
             'class_ids': predicted_classes[:, tf.newaxis],
             'probabilities': tf.keras.layers.Softmax(axis=1)(logits),
             'logits': logits
         }
         return tf.estimator.EstimatorSpec(mode, predictions=predictions)
-    
+
     xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels,
                                                               logits=logits)
-    loss =  tf.reduce_mean(xentropy)                                                     
+    loss = tf.reduce_mean(xentropy)
 
     accuracy = tf.compat.v1.metrics.accuracy(labels, predicted_classes, name='acc_op')
-    
+
     with tf.name_scope('metrics'):
         tf.compat.v1.summary.scalar('accuracy', accuracy[1])
-            
+
     metrics = {
         'metrics/accuracy': accuracy,
     }
-    
+
     if mode == tf.estimator.ModeKeys.EVAL:
         return tf.estimator.EstimatorSpec(mode,
                                           loss=loss,
@@ -79,14 +79,12 @@ def model_fn(features, labels, mode, params):
 
     assert mode == tf.estimator.ModeKeys.TRAIN
     optimizer = params['optimizer']
-    
+
     # get operations related to batch normalization
     # see: https://stackoverflow.com/questions/45299522/batch-normalization-in-a-custom-estimator-in-tensorflow
     # see: https://github.com/tensorflow/tensorflow/issues/16455
     update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
     with tf.control_dependencies(update_ops):
         train_op = optimizer.minimize(loss, global_step=tf.compat.v1.train.get_global_step())
-    
+
     return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
-
-
